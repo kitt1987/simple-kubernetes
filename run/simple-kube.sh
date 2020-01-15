6474acbf5a867afd58130dc7df5278fd6277b51f
Kitt Hsu <kitt.hsu@gmail.com>
2020-01-15 11:11:23 +0800

    supports remove masters

--- run/simple-kube.sh
+++ run/simple-kube.sh
@@ -7,4 +7,4 @@ docker run --name simple-kube --net=host -ti --rm \
   -e http_proxy="${http_proxy}" -e https_proxy="${https_proxy}" -e no_proxy="${no_proxy}" \
   --mount type=tmpfs,destination=/root/.ansible/cps \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  kitt0hsu/simple-kube:a795a4a3cc16ca33cd755b73beb16099cdff1df3-20200114163948
+  kitt0hsu/simple-kube:15415b795e7209af44b884dd44a1781552e77d6f-20200115112833
