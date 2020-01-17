1ef4919cf1a8d910c6cc14a3cc3db1d00b6473b6
Kitt Hsu <kitt.hsu@gmail.com>
2020-01-17 17:57:22 +0800

    support flannel

--- run/simple-kube.sh
+++ run/simple-kube.sh
@@ -7,4 +7,4 @@ docker run --name simple-kube --net=host -ti --rm \
   -e http_proxy="${http_proxy}" -e https_proxy="${https_proxy}" -e no_proxy="${no_proxy}" \
   --mount type=tmpfs,destination=/root/.ansible/cps \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  kitt0hsu/simple-kube:15415b795e7209af44b884dd44a1781552e77d6f-20200115112833
+  kitt0hsu/simple-kube:476d1a3bc00b38bb6dc23f3dde83d8dba0221727-20200116174724
