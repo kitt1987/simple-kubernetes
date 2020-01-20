ac9e07714595e5e60daa48e03e7a52aa10fd2ccb
Kitt Hsu <kitt.hsu@gmail.com>
2020-01-20 17:09:24 +0800

    all-in-one

---
+++ run/all-in-one-1.17.1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Mount tmpfs for SSH ControlPath to work around SSH failures on overlay.
+# See https://github.com/moby/moby/issues/12080
+
+docker run --name simple-kube --net=host -ti --rm \
+  -e http_proxy="${http_proxy}" -e https_proxy="${https_proxy}" -e no_proxy="${no_proxy}" \
+  --mount type=tmpfs,destination=/root/.ansible/cps \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  kitt0hsu/simple-kube:v1.17.1-all-in-one
