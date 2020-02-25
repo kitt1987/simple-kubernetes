28adea0d4ec8253656701235dfdca422f6d44dba
Kitt Hsu <kitt.hsu@gmail.com>
2020-02-25 14:53:55 +0800

    introduces factor cache

--- run/all-in-one-1.16.6.sh
+++ run/all-in-one-1.16.6.sh
@@ -5,6 +5,6 @@
 
 docker run --name simple-kube --net=host -ti --rm \
   -e http_proxy="${http_proxy}" -e https_proxy="${https_proxy}" -e no_proxy="${no_proxy}" \
-  --mount type=tmpfs,destination=/root/.ansible/cps \
+  --mount type=tmpfs,destination=/root/.ansible/cp \
   -v /var/run/docker.sock:/var/run/docker.sock \
   kitt0hsu/simple-kube:v1.16.6-all-in-one
--- run/debug-kube.sh
+++ run/debug-kube.sh
@@ -7,9 +7,9 @@ mkdir -p lib/on-prem-cache
 
 docker run --name simple-kube --net=host -ti --rm \
   -e http_proxy="${http_proxy}" -e https_proxy="${https_proxy}" -e no_proxy="${no_proxy}" \
-  --mount type=tmpfs,destination=/root/.ansible/cps \
+  --mount type=tmpfs,destination=/root/.ansible/cp \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(pwd)/lib/releases:/simple-kube/lib/releases \
   -v $(pwd)/inventory:/simple-kube/lib/inventory \
   -v $(pwd)/lib/on-prem-cache:/simple-kube/lib/on-prem-cache \
-  kitt0hsu/simple-kube:b811d1d76346ebabd13a7cec3109ff286c306b26-20200120141902
+  kitt0hsu/simple-kube
